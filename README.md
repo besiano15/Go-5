58ef7e1c34d265c19af4229fbf5ff3f20a1ee479
zaifeng <20012382@qq.com>
2015-08-22 23:57:38 +0800

    add study note

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Go Lang
 Google Go language study
+
+Go在默认的情况下，如果追加的元素超过了容量大小，Go会自动地重新为切片分配容量，容量大小为原来的两倍
--- array.go
+++ array.go
@@ -9,4 +9,32 @@ func main(){
     fmt.Println(a[0] , a[1])
 
     fmt.Println(a)
+
+    //array init
+    var x [5]int
+    x[0] = 1
+    x[1] = 2
+    for _,i := range x {
+        fmt.Println(i)
+    }
+
+    //array init 2
+    var y = [5]int{1,2}
+    fmt.Println(y)
+    
+    //array init 3
+    var z = [...]bool{true , false , true , false}
+    fmt.Println(z,len(z))
+    
+    //array init 4
+    var j = []string{
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",       //,必须，或写成"Saturday"}
+    }
+    fmt.Println(j)
 }
