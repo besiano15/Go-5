8df54cb98a605ad1da9fd59a3a5253b3db69e1b2
zaifeng <20012382@qq.com>
2015-10-26 16:52:18 +0800

    add init usage

--- init.go
+++ init.go
@@ -6,6 +6,16 @@ import (
 
 func main() {
     //var a , b ,c := false , true , "java" 
+    var x string = "hello world" //显式变量定义
+    var y string
+    y = "hello too" // 定义 赋值分离
+
+    var z = "hello either" //类型推导
+
+    q := "hello pg" //自动类型推导，不需要var，但必须在func内
     a , b ,c := false , true , "java" 
     fmt.Println(a,b,c)
+
+    fmt.Println(x,y,z,q)
+
 }
